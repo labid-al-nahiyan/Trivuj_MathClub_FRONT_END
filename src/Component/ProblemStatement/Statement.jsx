e559a3aa410da34b38bbf9dd5c7d7e83debf6dda
Labid Al Nahiyan <labid.nahiyan12@gmail.com>
2022-08-22 21:31:58 +0600

    submit update

--- src/Component/ProblemStatement/Statement.jsx
+++ src/Component/ProblemStatement/Statement.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import parse from 'html-react-parser';
 import './Statement.css'
 import NavBar from '../NavBar/NavBar';
+import { UserContext } from '../../App';
 
 const Statement = () => {
     
@@ -11,8 +12,11 @@ const Statement = () => {
     const [problem, setProblem] = useState({});
     const [string, setString ] = useState('');
     const [submit,setSubmit] = useState({});
-    const [ans,setAns] = useState(0);
+    const [loggedInUser,setLoggedInUser] = useContext(UserContext);
+    const [ans,setAns] = useState('');
     const parse = require('html-react-parser')
+    let navigate  = useNavigate();
+    let location = useLocation();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -21,41 +25,88 @@ const Statement = () => {
           const data = await res.json();
           setProblem(data[0]);
           setString(data[0].STATEMENT)
-          console.log(string)
           
-          
+          //console.log(data);    
         }      
         fetchData()
         .catch(console.error);
 
         
-
+        
     }, [])
+
+    useEffect(()=>{
+        postSubmit();
+    },[submit])
     
     const handleChange = async (event) => {
         
-        console.log(event.target.name, event.target.value)
-
-        console.log(ans);
-        console.log(problem.ANSWER);
+     
+        const newUserInfo = { ...submit }
+        newUserInfo['PROBLEMID'] =  problem.ID
+        newUserInfo['MEMBERID'] = loggedInUser.ID
+        setSubmit(newUserInfo)
 
         if(event.target.name==='answer'){
-           await setAns(event.target.value);
+            setAns(event.target.value);
         }
-  
+
     }
 
+    const postSubmit=()=>{
+        console.log(submit)
 
-    const handleSubmit=(e)=>{
-        console.log(typeof(ans));
-        console.log(typeof(problem.ANSWER));
-        if(ans==problem.ANSWER){
+        if(submit.hasOwnProperty('verdict') && submit.hasOwnProperty('PROBLEMID')){
+            console.log("complete")
+            try {
+                fetch('http://localhost:3010/submit', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(submit)
+                })
+                .then((res)=>{
+                    res.json()
+                    console.log('hello');
+                })
+                .then(data=>{
+                    console.log(data)
+                    console.log('hello2')
+                    navigate(location?.state?.from || '/', {replace:true})
+                })
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        else{
+            console.log('imcomplete')
+            
+        }
+        
+    }
+
+    const handleSubmit=async(e)=>{
+       
+
+        const submittedAns = ans*1;
+        const newUserInfo = { ...submit }
+        newUserInfo['submittedAns'] = submittedAns;
+
+        if(submittedAns===problem.ANSWER){
             console.log("Answer Match");
+            newUserInfo['verdict'] = 'T';
         }
         else{
             console.log("Answer Dont match")
+            newUserInfo['verdict'] = 'F';
         }
+        setSubmit(newUserInfo);
+        
+    
     }
+
     return (
         <div>
             <NavBar></NavBar>
